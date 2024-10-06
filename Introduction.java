20de74de1883dfebc53948392db67b39115e7852
Zenan0 <zenanl@nycstudents.net>
2024-10-06 23:53:33 +0000

    message

---
+++ Introduction.java
@@ -0,0 +1,19 @@
+/* Programmer: Zenan Li
+ * This class prints a message introducing myself and some things about me. It also includes some mathematical equations.
+*/
+
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.print("Hello. ");
+        System.out.println("My name is Zenan");
+        System.out.println("I like to play video games and hang out with my friends.");
+        System.out.println("I also like math so here are some equations:");
+        System.out.println();
+        System.out.print("10*10 = ");
+        System.out.println(100);
+        System.out.print("102493 - 5049 = ");
+        System.out.println(102493 - 5049); // Here I just copy and paste the equation from above and remove the parentheses to let the program calculate the answer for me.
+        System.out.print("1 + 1 = ");
+        System.out.println("2");
+    }
+}
---
+++ TurinCat.java
@@ -0,0 +1,7 @@
+public class TurinCat {
+    public static void main(String[] args) {
+        System.out.print("Hello");
+        System.out.println("What's up?");
+        System.out.println("Not much.");        
+    }
+}
